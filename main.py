544d2b1b2c58b7fead9a3a99c9d6c80279da03df
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-12-09 12:32:17 +0000

    Final commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Base(DeclarativeBase):
     pass
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'SomeKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_top_videogames.db'
 
 Bootstrap5(app)
